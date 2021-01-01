c75e872aefe58025ce4925db0bcce458a9e04a8f
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-01-01 15:53:53 -0600

    Going back to working with callbacks

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const data = require('./mongo')
 const path = require('path');
-const https = require('https')
 const app = express();
 const PORT = process.env.PORT || 8081
 

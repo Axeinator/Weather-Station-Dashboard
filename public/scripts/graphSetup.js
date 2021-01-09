be026fe27695c9fdab6b8268a14f2e907f6bad23
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-01-09 10:55:06 -0600

    Simplified codebase - no extra graphing file

--- public/scripts/graphSetup.js
+++ public/scripts/graphSetup.js
@@ -4,6 +4,114 @@ function toCelsius(temp) {
   return (temp - 32) * (5 / 9);
 }
 
+function temperatureGraph(xArray, yArray, suffix) {
+  const ctx = document.getElementById('temperatureChart');
+  const data = {
+    labels: xArray,
+    datasets: [
+      {
+        label: 'Temperature',
+        pointBackgroundColor: '#ff2200',
+        backgroundColor: 'rgba(255, 198, 133, 0.6)',
+        borderColor: '#ff6200',
+        data: yArray,
+      },
+    ],
+  };
+  return new Chart(ctx, {
+    type: 'line',
+    data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {},
+        }],
+        xAxes: [{
+          type: 'time',
+          time: {
+            unit: 'minute',
+          },
+          ticks: {
+            min: Date.now() - (86400 * 1000),
+            max: Date.now(),
+            maxTicksLimit: xArray.length / 15,
+
+          },
+        }],
+      },
+      maintainAspectRatio: false,
+      responsive: true,
+      tooltips: {
+        callbacks: {
+          label(tooltipItem, lab) {
+            let { label } = lab.datasets[tooltipItem.datasetIndex];
+            label += ': ';
+            label += Math.round(tooltipItem.value * 10) / 10;
+            label += suffix;
+            return label;
+          },
+        },
+      },
+    },
+  });
+}
+
+function humidityGraph(xArray, yArray) {
+  const ctx = document.getElementById('humidityChart');
+  const data = {
+    labels: xArray,
+    datasets: [
+      {
+        label: 'Humidity',
+        pointBackgroundColor: '#03a1fc',
+        backgroundColor: 'rgba(140, 183, 252, 0.5)',
+        borderColor: '#49bbfc',
+        data: yArray,
+      },
+    ],
+  };
+  return new Chart(ctx, {
+    type: 'line',
+    data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: 0,
+            max: 100,
+            stepSize: 5,
+          },
+        }],
+        xAxes: [{
+          type: 'time',
+          time: {
+            unit: 'minute',
+          },
+          ticks: {
+            min: Date.now() - (86400 * 1000),
+            max: Date.now(),
+            maxTicksLimit: xArray.length / 15,
+
+          },
+        }],
+      },
+      maintainAspectRatio: false,
+      responsive: true,
+      tooltips: {
+        callbacks: {
+          label(tooltipItem, lab) {
+            let { label } = lab.datasets[tooltipItem.datasetIndex];
+            label += ': ';
+            label += Math.round(tooltipItem.value * 10) / 10;
+            label += '%';
+            return label;
+          },
+        },
+      },
+    },
+  });
+}
+
 function temperatureGraphSetup(temps, suffix) {
   const xArray = [];
   temps.forEach((obs) => xArray.push(new Date(obs.time)));
